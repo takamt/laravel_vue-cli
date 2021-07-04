827d8fb3c959bf4162b5959b047962460a9e7533
takamt <tkn.moto@gmail.com>
2021-07-04 16:41:46 +0900

    SPA用のLaravelルーティング設定

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/{any}', function () {
+    return view('spa.main');
+})->where('any', '.*');
+
 Route::get('/', function () {
     return view('welcome');
 });
